52c638ba4bae768bedf0f8cf00b077c717ddcf4e
Divya Raj <rajdivya6533@gmail.com>
2026-03-11 08:49:25 +0530

    Switch to HashRouter for GitHub Pages support

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/smart-farming-app/">
+    <HashRouter>
       <App />
       <Toaster position="top-right" toastOptions={{
         style: {
@@ -18,6 +18,6 @@ createRoot(document.getElementById('root')).render(
           fontFamily: "'Inter', sans-serif",
         }
       }} />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
