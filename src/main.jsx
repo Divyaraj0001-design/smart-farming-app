62bee372de17cef3bebad033a5b6c0c8f2d2e485
Divya Raj <rajdivya6533@gmail.com>
2026-03-11 08:46:30 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/smart-farming-app/">
       <App />
       <Toaster position="top-right" toastOptions={{
         style: {
